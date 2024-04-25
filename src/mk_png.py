f88c9f62aa7f231f949d9f52ca5bd63429771c4f
Pete Ratzlaff <pratzlaff@cfa.harvard.edu>
2024-04-24 21:03:29 -0400

    PNG generation options

--- src/mk_png.py
+++ src/mk_png.py
@@ -8,7 +8,7 @@ import sys
 
 import HRCExp
 
-def mk_png(indir, outdir, det, subdet, type):
+def mk_png(indir, outdir, det, subdet, type, ds9, matplotlib, fitspng):
     HRCExp.mkdir_p(outdir)
 
     ifiles = glob.glob(f'{indir}/hrc{det}{subdet}{type}_[0-9][0-9][0-9][0-9]-[0-9][0-9].fits.gz')
@@ -21,7 +21,12 @@ def mk_png(indir, outdir, det, subdet, type):
         ofile = ofiles[i]
 
         sys.stderr.write(f'{ifile} -> {ofile}\n')
-        HRCExp.fits2png_matplotlib(ifile, ofile)
+        if (ds9):
+            HRCExp.fits2png_ds9(ifile, ofile)
+        if (matplotlib):
+            HRCExp.fits2png_matplotlib(ifile, ofile)
+        if (fitspng):
+            HRCExp.fits2png_fitspng(ifile, ofile)
 
 def main():
     parser = argparse.ArgumentParser(
@@ -32,8 +37,12 @@ def main():
     parser.add_argument('-d', '--det', default='[is]', help='Detector.')
     parser.add_argument('-s', '--subdet', default='[0-2]', help='Detector.')
     parser.add_argument('-t', '--type', default='[mc]', help='Detector.')
+    parser.add_argument('--ds9', action='store_true')
+    parser.add_argument('--matplotlib', action='store_true')
+    parser.add_argument('--fitspng', action='store_true')
+
     args = parser.parse_args()
-    mk_png(args.indir, args.outdir, args.det, args.subdet, args.type)
+    mk_png(args.indir, args.outdir, args.det, args.subdet, args.type, args.ds9, args.matplotlib, args.fitspng)
 
 if __name__ == '__main__':
     main()
